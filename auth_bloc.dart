779433cd14d8bf90a3b6e64e36514a711b77f98c
Estevan <50497584+estevang12@users.noreply.github.com>
2025-04-25 18:04:39 -0500

    Add files via upload

---
+++ auth_bloc.dart
@@ -0,0 +1,38 @@
+auth_bloc.dart
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+part 'auth_event.dart';
+part 'auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final AuthRepository _authRepository;
+
+  AuthBloc(this._authRepository) : super(AuthInitial()) {
+    on<SignInRequested>(_onSignInRequested);
+    on<SignOutRequested>(_onSignOutRequested);
+  }
+
+  Future<void> _onSignInRequested(
+    SignInRequested event,
+    Emitter<AuthState> emit,
+  ) async {
+    emit(AuthLoading());
+    try {
+      final user = await _authRepository.signInWithEmailAndPassword(
+        email: event.email,
+        password: event.password,
+      );
+      emit(Authenticated(user));
+    } catch (e) {
+      emit(AuthError(e.toString()));
+    }
+  }
+
+  Future<void> _onSignOutRequested(
+    SignOutRequested event,
+    Emitter<AuthState> emit,
+  ) async {
+    await _authRepository.signOut();
+    emit(Unauthenticated());
+  }
+}
